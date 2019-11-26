d72c322d0a541d912cce06b65d4cc727d6393dda
bryther <alec.christiaen1@live.be>
2019-11-26 09:46:08 +0100

    e:   bin/ArrayPrinter.class 	new file:   bin/module-info.class 	modified:   src/ArrayPrinter.java

--- src/ArrayPrinter.java
+++ src/ArrayPrinter.java
@@ -1,11 +1,23 @@
 
 public class ArrayPrinter {
+	static int[] sloeber = new int [5];
 	
 	public static void main (String[]args) {
-		int[] sloeber = new int [5];
+		ArrayPrinter toets = new ArrayPrinter();
+		sloeber[1] = 2;
+		sloeber[2] = 5;
+		sloeber[3] = 3;
+		sloeber[4] = 8;
+		sloeber[5] = 9;
+		toets.printarray();
+		
+		
 		
 	}
 	public void printarray() {
+		for (int i = 0; i < sloeber.length; i++) {
+			System.out.println(sloeber[i]);
+		}
 		
 	}
 }
